70786d1932c682a0555b6b968047ba8cce431d78
HuyGiaJerry <huyfin2004@gmail.com>
2025-04-08 09:40:18 +0700

    Chap 5: Nested Routes, Outlet, NavLink

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import './App.scss';
 import Header from './components/Header/Header';
-import {Link} from 'react-router-dom';
+import { Link ,Outlet} from 'react-router-dom';
 const App = () => {
 
-  return (  
+  return (
 
     <div className='app-container'>
-      <Header />
-      <div>test link</div>
-      <div>
-        <button>
-          <Link to='/user'>go to user page</Link>
-        </button>
-        <button>
-          <Link to='/admin'>go to admin page</Link>
-        </button>
+
+      <div className='header-container'>
+        <Header />
+      </div>
+      <div className='main-container'>
+        <div className='sidebar-container'> </div>
+        <div className='app-content'>
+          <Outlet />
+          
+        </div>
       </div>
     </div>
   );
